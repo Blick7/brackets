99ecca478f44708cf7c57368433129a2d8a39fa9
Blick7 <dima.8m8@gmail.com>
2021-03-12 14:25:41 +0200

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let joined = bracketsConfig.map(item => item.join(''));
+  let buff;
+  for (let i = 0; i < str.length + joined.length; i++) {
+    joined.forEach(item => {
+      while (str.includes(item)) {
+        str = str.replace(item, '');
+        //console.log(str);
+      }
+    });
+  }
+  console.log(joined);
+  return str.length == 0;
 }
+
+console.log(module.exports('([{}])', [['(', ')'], ['[', ']'], ['{', '}']])); // true
